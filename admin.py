1fe585133c1fcddefd666631ed4809e3bbd9b2c3
Shen <etkids@163.com>
2016-02-08 18:40:29 -0500

    Basic admin settings

--- admin.py
+++ admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Topic, Node, Post
 
 # Register your models here.
+
+
+class PostInline(admin.TabularInline):
+    model = Post
+    fields = ('user', 'content_raw')
+
+
+class TopicAdmin(admin.ModelAdmin):
+    inlines = [
+        PostInline
+    ]
+
+admin.site.register(Topic, TopicAdmin)
+admin.site.register(Node, TopicAdmin)
