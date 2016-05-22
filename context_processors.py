7e227eb6d2f78cc8d87f4401745138e578f8d448
Shen <etkids@163.com>
2016-05-21 23:43:10 -0400

    show site_name in html title

--- context_processors.py
+++ context_processors.py
@@ -1,11 +1,14 @@
 # -*- coding: utf-8 -*-
 from .models import Node
+from django.utils.translation import ugettext as _
+from django.conf import settings
 
 
 def niji_processor(request):
     nodes = Node.objects.all()
+    site_name = _(getattr(settings, 'NIJI_SITE_NAME', ''))
     try:
         unread_count = request.user.received_notifications.filter(read=False).count()
     except AttributeError:
         unread_count = None
-    return {'nodes': nodes, 'unread_count': unread_count}
+    return {'nodes': nodes, 'unread_count': unread_count, 'site_name': site_name}
